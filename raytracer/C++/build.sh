3e038d4db5b8dfc814e8e8f509f343c3cb7d72e8
Jakub Profota <142504264+profojak@users.noreply.github.com>
2024-06-29 21:08:37 +0200

    ⚙️ raytracer/C++: Update build script

--- raytracer/C++/build.sh
+++ raytracer/C++/build.sh
@@ -30,11 +30,17 @@ mkdir -p bin/
 (cd $LIB && exe ./build.sh)
 exe clang++ $FLAGS -x c++-module include/Random.ccm --precompile $MODULES -o bin/Random.pcm
 exe clang++ $FLAGS -x c++-module include/Ray.ccm --precompile $MODULES -o bin/Ray.pcm
+exe clang++ $FLAGS -x c++-module include/Material.ccm --precompile $MODULES -o bin/Material.pcm
+exe clang++ $FLAGS -x c++-module include/Object.ccm --precompile $MODULES -o bin/Object.pcm
 exe clang++ $FLAGS -x c++-module include/Camera.ccm --precompile $MODULES -o bin/Camera.pcm
+exe clang++ $FLAGS src/Material.cc $MODULES -c -o bin/Material-src.o
+exe clang++ $FLAGS src/Object.cc $MODULES -c -o bin/Object-src.o
 exe clang++ $FLAGS src/Camera.cc $MODULES -c -o bin/Camera-src.o
 exe clang++ $FLAGS src/main.cc $MODULES -c -o bin/main.o
 exe clang++ $FLAGS bin/Random.pcm $MODULES -c -o bin/Random.o
 exe clang++ $FLAGS bin/Ray.pcm $MODULES -c -o bin/Ray.o
+exe clang++ $FLAGS bin/Material.pcm $MODULES -c -o bin/Material.o
+exe clang++ $FLAGS bin/Object.pcm $MODULES -c -o bin/Object.o
 exe clang++ $FLAGS bin/Camera.pcm $MODULES -c -o bin/Camera.o
-exe clang++ bin/main.o bin/Camera.o bin/Random.o bin/Ray.o bin/Camera-src.o -o raytracer
+exe clang++ bin/main.o bin/Camera.o bin/Camera-src.o bin/Material.o bin/Material-src.o bin/Object.o bin/Object-src.o bin/Random.o bin/Ray.o -o raytracer
 exit 0
