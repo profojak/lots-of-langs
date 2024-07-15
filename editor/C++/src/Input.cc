d30fdbef3fda642f288321c7b6e4720860157c08
Jakub Profota <142504264+profojak@users.noreply.github.com>
2024-07-15 08:21:16 +0200

    ⭐ editor/C++: Render welcome message

--- editor/C++/src/Input.cc
+++ editor/C++/src/Input.cc
@@ -1,6 +1,7 @@
 module;
 
 #include <iostream>
+#include <ranges>
 
 import Terminal;
 
@@ -8,11 +9,24 @@ module Input;
 
 namespace editor {
 
+/*! @brief Tab spaces. */
+constexpr const auto TAB_SPACES = 8;
+
 /*! @brief Number of confirmations to quit. */
 //constexpr const auto QUIT_TIMES = 3;
 
 /*! @brief Control key. */
 const auto CONTROL_KEY = [](const auto key) { return key & 0x1f; };
+
+auto Line::CursorXToRenderedX(int cursor_x) noexcept -> int {
+    auto rendered_x = 0;
+    for(const auto& j : std::ranges::views::iota(0, cursor_x)) {
+        if(characters[j] == '\t')
+            rendered_x += (TAB_SPACES - 1) - (rendered_x % TAB_SPACES);
+        rendered_x++;
+    }
+    return rendered_x;
+}
 
 void ProcessKeypress() {
     auto c = ReadKey();
--- editor/C++/src/Output.cc
+++ editor/C++/src/Output.cc
@@ -1,5 +1,6 @@
 module;
 
+#include <chrono>
 #include <iostream>
 #include <ranges>
 #include <string>
@@ -10,10 +11,48 @@ module Output;
 
 namespace editor {
 
+/*! @brief Editor version. */
+constexpr const std::string_view VERSION{"0.0.1"};
+
+void Scroll() {
+    editor.rendered_x = 0;
+    if(editor.cursor_y < editor.lines_count)
+        editor.rendered_x = editor.lines[editor.cursor_y].CursorXToRenderedX(
+            editor.cursor_x);
+    
+    if(editor.cursor_y < editor.row_offset)
+        editor.row_offset = editor.cursor_y;
+    if(editor.cursor_y >= editor.row_offset + editor.screen_rows)
+        editor.row_offset = editor.cursor_y - editor.screen_rows + 1;
+    if(editor.rendered_x < editor.col_offset)
+        editor.col_offset = editor.rendered_x;
+    if(editor.rendered_x >= editor.col_offset + editor.screen_cols)
+        editor.col_offset = editor.rendered_x - editor.screen_cols + 1;
+}
+
 void DrawLines(std::string& buffer) {
-    for([[maybe_unused]] const auto& y :
-        std::ranges::views::iota(0, editor.screen_rows)) {
-        buffer.append("~");
+    for(const auto& y : std::ranges::views::iota(0, editor.screen_rows)) {
+        auto file_line = y + editor.row_offset;
+        if(file_line >= editor.lines_count) {
+            if(editor.lines_count == 0 && y == editor.screen_rows / 3) {
+                std::string welcome = "Text editor - version " +
+                    std::string{ VERSION };
+                if(welcome.size() >
+                    static_cast<unsigned long>(editor.screen_cols))
+                    welcome.resize(editor.screen_cols);
+                auto padding = (editor.screen_cols - welcome.size()) / 2;
+                if(padding) {
+                    buffer.append("~");
+                    padding--;
+                }
+                buffer.append(std::string(padding, ' '));
+                buffer.append(welcome);
+            } else
+                buffer.append("~");
+        } else {
+
+        }
+
         buffer.append("\x1b[K\r\n");
     }
 }
@@ -24,10 +63,17 @@ void DrawStatusBar(std::string& buffer) {
 
 void DrawMessageBar(std::string& buffer) {
     buffer.append("\x1b[K");
-    buffer.append(editor.status_message);
+    if(editor.status_message.size() >
+        static_cast<unsigned long>(editor.screen_cols))
+        editor.status_message.resize(editor.screen_cols);
+    auto now = std::chrono::system_clock::now();
+    if(std::chrono::system_clock::to_time_t(now) - editor.status_time < 5)
+        buffer.append(editor.status_message);
 }
 
 void RefreshScreen() {
+    Scroll();
+
     std::string buffer;
     buffer.append("\x1b[?25l\x1b[H");
 
