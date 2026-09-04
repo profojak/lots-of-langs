7c8cc6d37293b907db2c6c7d4fd8742a45b3e7be
Jakub Profota <245464941+profojak@users.noreply.github.com>
2026-09-04 20:28:16 +0200

    💎 C++: Hook up headless and interface runs

--- langs/C++/src/main.cpp
+++ langs/C++/src/main.cpp
@@ -1,15 +1,32 @@
-#include <raylib.h>
+#include <print>
 
+import argument;
+import configuration;
+import particle;
+import solver;
 import vector;
+import renderer;
 
-int main() {
-  InitWindow(800, 600, "Position Based Fluids in C++23");
-
-  while (!WindowShouldClose()) {
-    BeginDrawing();
-    ClearBackground(RAYWHITE);
-    EndDrawing();
+int main(int argc, char **argv) {
+  pbf::Arguments arguments;
+  try {
+    arguments = pbf::ParseArguments({argv + 1, argv + argc});
+  } catch (const std::exception &e) {
+    std::println("Argument error: {}", e.what());
+    return 1;
   }
 
-  CloseWindow();
+  pbf::Configuration configuration;
+  pbf::Particles particles(configuration.particles);
+  pbf::PBFSolver solver{configuration};
+
+  if (arguments.no_gui) {
+    for (unsigned step = 0; step < configuration.parameters.simulation_steps; ++step)
+      solver.Step(particles);
+  } else {
+    pbf::Renderer renderer{configuration};
+    renderer.Run(particles, solver);
+  }
+
+  return 0;
 }
